a6a070deca0a3d381fc05d844fdc6e5c78fef336
Dhirajlal Vishwakarma <91944134+vidhiraj201@users.noreply.github.com>
2021-11-29 18:02:55 +0530

    new Update

--- Assets/_Game/_Scripts/Core/Utilities/StoreExpansion.cs
+++ Assets/_Game/_Scripts/Core/Utilities/StoreExpansion.cs
@@ -11,9 +11,13 @@ namespace FashionM.Core
     {
         public LevelManagerStore lv;
         public TextMeshPro StoreStatus;
-        public TextMeshPro StoreUpgradesPrice;
 
+        public TextMeshPro StoreUpgradesPrice;
+        public TextMeshPro StoreUpgradesPrice_1;
         public Image WaitTimerUnlockUI;
+        public Image WaitTimerUnlockUI_1;
+
+
         public float WaitTimer=1;
         private float UIUnlock;
         private bool isPlayerNear;
@@ -31,6 +35,7 @@ namespace FashionM.Core
         public float xPos;
         public float zPos;
         public Vector3 PlacingRotation;
+        public Vector3 PlacingPosition;
 
         [Header ("Collider Bound")]
         public Vector3 BoundCenter;
@@ -41,9 +46,16 @@ namespace FashionM.Core
         {
             GM = FindObjectOfType<GameManager>();
             Bounds = GM.Bound;
-            StoreStatus.text = "Store Locked";
+            //StoreStatus.text = "Store Locked";
             StoreUpgradesPrice.text = "$" + MaxCoinNeedToUnlock;
+            StoreUpgradesPrice_1.text = "$" + MaxCoinNeedToUnlock;
+
+
+            StoreUpgradesPrice.gameObject.SetActive(false);
+            StoreUpgradesPrice_1.gameObject.SetActive(false);
             WaitTimerUnlockUI.gameObject.SetActive(false);
+            WaitTimerUnlockUI_1.gameObject.SetActive(false);
+
             UIUnlock = WaitTimer;
 
             xPos = -55.5f;
@@ -66,19 +78,19 @@ namespace FashionM.Core
                 /*Bounds.transform.localScale = new Vector3(24,10,131);
                 Bounds.transform.localPosition = new Vector3(-35.76f,39.3f,-36.45f);*/
 
-            /*    if (X)
-                {
-                    Bounds.GetComponent<BoxCollider>().center = new Vector3(BoundCenter.x, Bounds.GetComponent<BoxCollider>().center.y, Bounds.GetComponent<BoxCollider>().center.z); 
-                    Bounds.GetComponent<BoxCollider>().size = new Vector3(BoundSize.x, Bounds.GetComponent<BoxCollider>().size.y, Bounds.GetComponent<BoxCollider>().size.z); 
-                }
+                    if (X)
+                    {
+                        Bounds.GetComponent<BoxCollider>().center = new Vector3(BoundCenter.x, Bounds.GetComponent<BoxCollider>().center.y, Bounds.GetComponent<BoxCollider>().center.z); 
+                        Bounds.GetComponent<BoxCollider>().size = new Vector3(BoundSize.x, Bounds.GetComponent<BoxCollider>().size.y, Bounds.GetComponent<BoxCollider>().size.z); 
+                    }
 
-                if (Z)
-                {
-                    Bounds.GetComponent<BoxCollider>().center = new Vector3(Bounds.GetComponent<BoxCollider>().center.x, Bounds.GetComponent<BoxCollider>().center.y, BoundCenter.z ); 
-                    Bounds.GetComponent<BoxCollider>().size = new Vector3(Bounds.GetComponent<BoxCollider>().size.x, Bounds.GetComponent<BoxCollider>().size.y, BoundSize.z); 
-                }*/
+                    if (Z)
+                    {
+                        Bounds.GetComponent<BoxCollider>().center = new Vector3(Bounds.GetComponent<BoxCollider>().center.x, Bounds.GetComponent<BoxCollider>().center.y, BoundCenter.z ); 
+                        Bounds.GetComponent<BoxCollider>().size = new Vector3(Bounds.GetComponent<BoxCollider>().size.x, Bounds.GetComponent<BoxCollider>().size.y, BoundSize.z); 
+                    }
 
-                transform.GetComponent<MeshRenderer>().enabled = false;
+                //transform.GetComponent<MeshRenderer>().enabled = false;
                 Destroy(this.gameObject);
             }
             OpenUI();
@@ -95,17 +107,25 @@ namespace FashionM.Core
             if (isPlayerNear)
             {
                 UIUnlock -= Time.deltaTime;
+                
+
                 WaitTimerUnlockUI.gameObject.SetActive(true);
+                WaitTimerUnlockUI_1.gameObject.SetActive(true);
+
+
+                WaitTimerUnlockUI_1.fillAmount = UIUnlock / WaitTimer;
                 WaitTimerUnlockUI.fillAmount = UIUnlock / WaitTimer;
                 if (UIUnlock <= 0)
                 {
                     isPlayerNear = false;
                     UIUnlock = WaitTimer;
                     Spwan();
-                    WaitTimerUnlockUI.gameObject.SetActive(true);
-                    //GM.UnlockStoreExpansionUI.SetActive(true);
+                  /*  WaitTimerUnlockUI.gameObject.SetActive(false);
+                    if (WaitTimerUnlockUI_1 != null)
+                        WaitTimerUnlockUI_1.gameObject.SetActive(false);*/
+                        //GM.UnlockStoreExpansionUI.SetActive(true);
 
-                }
+                    }
             }
         }
 
@@ -114,12 +134,20 @@ namespace FashionM.Core
             if (collision.gameObject.CompareTag("Player"))
             {
                 isPlayerNear = true;
+                StoreUpgradesPrice.gameObject.SetActive(true);
+                StoreUpgradesPrice_1.gameObject.SetActive(true);
             }
         }
-        private void OnCollisionStay(Collision collision)
-        {
 
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                StoreUpgradesPrice.gameObject.SetActive(true);
+                StoreUpgradesPrice_1.gameObject.SetActive(true);
+            }
         }
+
         private void OnCollisionExit(Collision collision)
         {
             
@@ -128,9 +156,17 @@ namespace FashionM.Core
                 isPlayerNear = false;                
                 UIUnlock = WaitTimer;
             }
+            if (StoreUpgradesPrice.gameObject.activeSelf)
+                StoreUpgradesPrice.gameObject.SetActive(true);
 
-            if(WaitTimerUnlockUI.gameObject.activeSelf)
+            if (StoreUpgradesPrice_1.gameObject.activeSelf)
+                StoreUpgradesPrice_1.gameObject.SetActive(true);
+
+            if (WaitTimerUnlockUI.gameObject.activeSelf)
                 WaitTimerUnlockUI.gameObject.SetActive(false);
+
+            if(WaitTimerUnlockUI_1 != null &&WaitTimerUnlockUI_1.gameObject.activeSelf)
+                WaitTimerUnlockUI_1.gameObject.SetActive(false);
 
             if (GM.UnlockStoreExpansionUI.activeSelf)
                 GM.UnlockStoreExpansionUI.GetComponent<Animator>().Play("Out");
@@ -139,28 +175,42 @@ namespace FashionM.Core
 
         [Header("Temp")]
         public int StationCountData;
+        public GameObject ToSpwan;
         public void Spwan()
         {
-            if (facingSide == 1)
+            if (GM.MaxCoin >= MaxCoinNeedToUnlock && MaxCoinNeedToUnlock >= 0)
             {
-                if (GM.MaxCoin >= MaxCoinNeedToUnlock && MaxCoinNeedToUnlock >= 0)
-                {
-                    Instantiate(GM.LeftRoadFacingStation[StationCountData], lv.transform.position - new Vector3(xPos, 0, 0), Quaternion.Euler(PlacingRotation)).transform.parent = GameObject.Find("Expanded Store").transform;
-                    GM.MaxCoin -= MaxCoinNeedToUnlock;
-                    MaxCoinNeedToUnlock = 0;
-                    GM.UnlockStoreExpansionUI.gameObject.GetComponent<Animator>().Play("Out");                    
-                }
+                Instantiate(ToSpwan, PlacingPosition, Quaternion.Euler(PlacingRotation)).transform.parent = GameObject.Find("Expanded Store").transform;
+                GM.MaxCoin -= MaxCoinNeedToUnlock;
+                MaxCoinNeedToUnlock = 0;
+                //GM.UnlockStoreExpansionUI.gameObject.GetComponent<Animator>().Play("Out");
             }
-            if (facingSide == 2)
-            {
-                if (GM.MaxCoin >= MaxCoinNeedToUnlock && MaxCoinNeedToUnlock >= 0)
-                {
-                    Instantiate(GM.RightRoadFacingStations[StationCountData], lv.transform.position - new Vector3(0, 0, zPos), Quaternion.Euler(PlacingRotation)).transform.parent = GameObject.Find("Expanded Store").transform;
-                    GM.MaxCoin -= MaxCoinNeedToUnlock;
-                    MaxCoinNeedToUnlock = 0;
-                    GM.UnlockStoreExpansionUI.gameObject.GetComponent<Animator>().Play("Out");                    
-                }
-            }
+
+
+
+
+
+
+            /* if (facingSide == 1)
+             {
+                 if (GM.MaxCoin >= MaxCoinNeedToUnlock && MaxCoinNeedToUnlock >= 0)
+                 {
+                     Instantiate(GM.LeftRoadFacingStation[StationCountData], lv.transform.position - new Vector3(xPos, 0, 0), Quaternion.Euler(PlacingRotation)).transform.parent = GameObject.Find("Expanded Store").transform;
+                     GM.MaxCoin -= MaxCoinNeedToUnlock;
+                     MaxCoinNeedToUnlock = 0;
+                     GM.UnlockStoreExpansionUI.gameObject.GetComponent<Animator>().Play("Out");                    
+                 }
+             }
+             if (facingSide == 2)
+             {
+                 if (GM.MaxCoin >= MaxCoinNeedToUnlock && MaxCoinNeedToUnlock >= 0)
+                 {
+                     Instantiate(GM.RightRoadFacingStations[StationCountData], lv.transform.position - new Vector3(0, 0, zPos), Quaternion.Euler(PlacingRotation)).transform.parent = GameObject.Find("Expanded Store").transform;
+                     GM.MaxCoin -= MaxCoinNeedToUnlock;
+                     MaxCoinNeedToUnlock = 0;
+                     GM.UnlockStoreExpansionUI.gameObject.GetComponent<Animator>().Play("Out");                    
+                 }
+             }*/
         }
     }
 }
